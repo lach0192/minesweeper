f541e437fbbd7936d5a4e7af0b10fd12bb6b2244
lach0192 <lach0192@algonquinlive.com>
2018-05-10 17:47:33 -0400

    Fixed admin page
    Admin page wasn’t showing before, now it is.

--- MineSweepDjango/myenv/minesweeper/minesweeper/urls.py
+++ MineSweepDjango/myenv/minesweeper/minesweeper/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^', TemplateView.as_view(template_name="main.html")),
     url(r'^admin/', admin.site.urls),
+    url(r'^', TemplateView.as_view(template_name="main.html")),
 ]
